8b097669bcea4264bd599fcaded43b164e45a99e
azazelo <atvai@inbox.ru>
2022-01-06 12:40:12 +0300

    feat:create new js file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+    if(0 > n){
+    let ar = n * (-1);
+    let tar = String(ar).split('');
+    let tan = tar.reverse().join('');
+    return Number(tan);
+    }
+    else{
+    let tar = String(n).split('');
+    let tan = tar.reverse().join('');
+    return Number(tan);
+    } 
 }
